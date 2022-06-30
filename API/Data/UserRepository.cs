aab95f9fd7358a8398faecfb4ba82b01145de3da
NegruRomi <romina.negru@emea.nttdata.com>
2022-06-30 10:16:26 +0300

    End of section 18 (without messages and likes).

--- API/Data/UserRepository.cs
+++ API/Data/UserRepository.cs
@@ -41,10 +41,11 @@ namespace API.Data
             .SingleOrDefaultAsync(x => x.UserName == username);
         }
 
-        public async Task<bool> SaveAllAsync()
+        /*public async Task<bool> SaveAllAsync()
         {
             return await _context.SaveChangesAsync() > 0;
         }
+        */
 
         public void Update(AppUser user)
         {
@@ -78,6 +79,13 @@ namespace API.Data
             .Where(x => x.UserName == username)
             .ProjectTo<MemberDto>(_mapper.ConfigurationProvider)
             .SingleOrDefaultAsync();
+        }
+
+        public async Task<string> GetUserGender(string username)
+        {
+            return await _context.Users
+                .Where(x => x.UserName == username)
+                .Select(x => x.Gender).FirstOrDefaultAsync();
         }
     }
 }
